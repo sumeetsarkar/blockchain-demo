d41cd7e4ec69cccf73ae1eabe3f27031a62c272b
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2018-10-18 19:49:32 +0530

    yaml config for blockchain config & saves transactions to file

--- lib/blockchain.py
+++ lib/blockchain.py
@@ -2,28 +2,74 @@
 BlockChain
 """
 
+import os
 import json
+import yaml
 from .block import Block
 from .ledgerentry import LedgerEntry
 
 class BlockChain:
 
-  def __init__(self, name, difficulty, reward, filePath):
-    self.__name = name
-    self.__previousHashForGenesis = -1
+  def __init__(self, configPath):
+    if configPath is None:
+      raise Exception('Config file Path missing')
+    self.__configPath = configPath
+
+
+  def __enter__(self):
+    try:
+      # read yaml file and load
+      with open(self.__configPath) as f:
+        self.__ymlConfig = next(yaml.load_all(f))
+        self.__applyConfig()
+        return self
+    except Exception as e:
+      print('Error reading config file', self.__configPath, e)
+
+
+  def __exit__(self, *args):
+    self.dump_blockchain_to_file()
+
+
+  def __applyConfig(self):
+    # initialize empty chain
     self.__chain = []
-    self.__difficulty = difficulty
-    self.__minerReward = reward
-    if filePath:
-      fileData = ''
-      with open(filePath, 'rt') as f:
-        for l in f.readlines():
-          fileData += l
-        jsonArray = json.loads(fileData)
-        for jsonBlock in jsonArray:
-          self.__chain.append(Block(jsonBlock))
+    # load values from yaml file
+    self.__name = self.__ymlConfig['name']
+    self.__difficulty = self.__ymlConfig['difficulty']
+    self.__minerReward = self.__ymlConfig['reward']
+    self.__previousHashForGenesis = self.__ymlConfig.get('genesisPreviousHash', -1)
+    self.__testMode = self.__ymlConfig['testMode']
+    # check if file dump config is specified, mandatory if testMode is False
+    if self.__ymlConfig.get('dump') is None and self.__testMode is False:
+      raise Exception('No file dump information specified in config')
+    if self.__testMode is False:
+      dirname = self.__ymlConfig.get('dump')['dir']
+      filename = self.__ymlConfig.get('dump').get('file')
+      if filename is None:
+        filename = self.__name
+      filename += '.json'
+      # form file dump path
+      self.__filePath = os.path.join(os.path.dirname(__file__), '../', dirname, filename)
+      self.__load_blockchain_from_file()
     else:
       self.__chain.append(self.__create_genesis_block())
+
+
+  def __load_blockchain_from_file(self):
+    """Loads previously saved blockchain json from file
+    If file is empty/ or read error, creates genesis block and starts the chain
+    """
+    try:
+      with open(self.__filePath, 'rt') as f:
+        jsonArray = json.load(f)
+        for jsonBlock in jsonArray:
+          self.__chain.append(Block(jsonBlock))
+    except Exception as e:
+      print('Error loading blockchain from file', self.__filePath, e)
+    finally:
+      if len(self.__chain) == 0:
+        self.__chain.append(self.__create_genesis_block())
 
 
   # private method
@@ -60,6 +106,7 @@ class BlockChain:
     newBlock.mine()
     # append newly mined block to the block chain
     self.__chain.append(newBlock)
+    # self.dump_blockchain_to_file()
 
 
   def is_valid(self):
@@ -73,6 +120,21 @@ class BlockChain:
         return False, block
       previousHash = block.hash
     return True, None
+
+
+  def dump_blockchain_to_file(self):
+    if self.__filePath is None:
+      return
+    jsonArray = []
+    for block in self.__chain:
+      parsedTransaction = []
+      for tran in block.listOfTransaction:
+        parsedTransaction.append(tran.summary)
+      parsedData = json.loads(json.dumps(block.summary))
+      parsedData['transactions'] = parsedTransaction
+      jsonArray.append(parsedData)
+    with open(self.__filePath, 'w+') as f:
+      json.dump(jsonArray, f, indent=2)
 
 
   def list_chain(self):
@@ -94,7 +156,7 @@ class BlockChain:
         if tran.fromUser == user and tran.amount > 0:
           totalDebit -= tran.amount
           print(tran.summary)
-        elif tran.toUser is user and tran.amount > 0:
+        elif tran.toUser == user and tran.amount > 0:
           totalCredit += tran.amount
           print(tran.summary)
     print('Credits  : ' + str(totalCredit))
--- main.py
+++ main.py
@@ -1,36 +1,44 @@
+"""
+Demonstrates usage of the BlockChain demo library
+"""
+
+import os
 from lib import BlockChain
 from lib import LedgerEntry
 
 # create block chain instance with difficulty 1 and reward 10 units
-blockChain = BlockChain('sumeet-blockchain', 4, 10, None)
+configPath = os.path.join(os.path.dirname(__file__), 'config.yml')
 
-# create list of transactions from ledger entry
-listOfTransactions = []
-listOfTransactions += LedgerEntry('userA', 'userB', 400).transactions
-listOfTransactions += LedgerEntry('userA', 'userC', 200).transactions
-# add list of transactions to the block chain
-blockChain.add_block(listOfTransactions, 'sumeetsarkar')
+with BlockChain(configPath) as blockChain:
+  # create list of transactions from ledger entry
+  listOfTransactions = []
+  listOfTransactions += LedgerEntry('userA', 'userB', 400).transactions
+  listOfTransactions += LedgerEntry('userA', 'userC', 200).transactions
+  # add list of transactions to the block chain
+  blockChain.add_block(listOfTransactions, 'sumeetsarkar')
 
-# list the entire block chain so far
-print('\n\nBlock chain so far...')
-print('---------------------')
-blockChain.list_chain()
+  # list the entire block chain so far
+  print('\n\nBlock chain so far...')
+  print('---------------------')
+  blockChain.list_chain()
 
-# create list of transactions from ledger entry
-listOfTransactions = []
-listOfTransactions += LedgerEntry('userA', 'userB', 900).transactions
-listOfTransactions += LedgerEntry('userC', 'userD', 500).transactions
-# add list of transactions to the block chain
-blockChain.add_block(listOfTransactions, 'sumeetsarkar')
+  # create list of transactions from ledger entry
+  listOfTransactions = []
+  listOfTransactions += LedgerEntry('userA', 'userB', 900).transactions
+  listOfTransactions += LedgerEntry('userC', 'userD', 500).transactions
+  # add list of transactions to the block chain
+  blockChain.add_block(listOfTransactions, 'sumeetsarkar')
 
-# list the entire block chain so far
-print('\n\nBlock chain so far...')
-print('---------------------')
-blockChain.list_chain()
+  # list the entire block chain so far
+  print('\n\nBlock chain so far...')
+  print('---------------------')
+  blockChain.list_chain()
 
-# check the validity of the block chain
-print('\n\nBlock chain validity', blockChain.is_valid())
+  # check the validity of the block chain
+  print('\n\nBlock chain validity', blockChain.is_valid())
 
-blockChain.compute_statement_for_user('userA')
-blockChain.compute_statement_for_user('userC')
-blockChain.compute_statement_for_user('sumeetsarkar')
+  blockChain.compute_statement_for_user('userA')
+  blockChain.compute_statement_for_user('userB')
+  blockChain.compute_statement_for_user('userC')
+  blockChain.compute_statement_for_user('userD')
+  blockChain.compute_statement_for_user('sumeetsarkar')
